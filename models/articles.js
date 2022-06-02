871f2c3e980e2d168b9ffabef1b5ceaa4f5422aa
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-06-02 09:26:31 -0700

    style authors show and edit pages

--- models/articles.js
+++ models/articles.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
+	img: String,
 	title: String,
     author: String,
 	body: String
--- models/authors.js
+++ models/authors.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const authorSchema = new Schema({
-    name: String
+    img: String,
+    name: String,
+    bio: String
 }, {
     timestamps: true
 });
